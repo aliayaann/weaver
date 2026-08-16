7e71320959edeb8373b2da2e155f0da6c48cc95e
aliayaann <aliayaan.personal@gmail.com>
2026-08-16 11:11:39 +0500

    feat(dom): add attribute map to element nodes

---
+++ build/CMakeFiles/mybrowser.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mybrowser.dir/src/html/lexer.cpp.obj"
+  "CMakeFiles/mybrowser.dir/src/html/lexer.cpp.obj.d"
+  "CMakeFiles/mybrowser.dir/src/html/parser.cpp.obj"
+  "CMakeFiles/mybrowser.dir/src/html/parser.cpp.obj.d"
+  "CMakeFiles/mybrowser.dir/src/main.cpp.obj"
+  "CMakeFiles/mybrowser.dir/src/main.cpp.obj.d"
+  "libmybrowser.dll.a"
+  "mybrowser.exe"
+  "mybrowser.exe.manifest"
+  "mybrowser.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mybrowser.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

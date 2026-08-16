af0684795a620f53ce7f6893edd8330153ce7b0c
aliayaann <aliayaan.personal@gmail.com>
2026-08-16 11:23:47 +0500

    feat(parser): implement robust attribute parsing state machine

--- src/html/parser.cpp
+++ src/html/parser.cpp
@@ -1,58 +1,93 @@
 #include "parser.h"
 #include <stack>
+#include <cctype>
 
 Parser::Parser(std::vector<Token> tokens) : tokens(tokens), pos(0) {}
 
-std::string Parser::extract_tag_name(const std::string& raw_tag) {
-    // If the tag has spaces (like "div class='box'"), we only want "div"
-    size_t space_pos = raw_tag.find(' ');
-    if (space_pos == std::string::npos) {
-        return raw_tag; 
+std::pair<std::string, std::map<std::string, std::string>> Parser::parse_tag(const std::string& raw_tag) {
+    std::string tag_name;
+    std::map<std::string, std::string> attrs;
+    
+    size_t i = 0;
+    // 1. Extract tag name (skip leading whitespace)
+    while(i < raw_tag.length() && std::isspace(raw_tag[i])) i++;
+    size_t start = i;
+    while(i < raw_tag.length() && !std::isspace(raw_tag[i])) i++;
+    tag_name = raw_tag.substr(start, i - start);
+    
+    // 2. Extract attributes
+    while(i < raw_tag.length()) {
+        // Skip whitespace
+        while(i < raw_tag.length() && std::isspace(raw_tag[i])) i++;
+        if(i >= raw_tag.length()) break;
+        
+        // Read attribute name
+        size_t key_start = i;
+        while(i < raw_tag.length() && raw_tag[i] != '=' && !std::isspace(raw_tag[i])) i++;
+        std::string key = raw_tag.substr(key_start, i - key_start);
+        
+        // Skip whitespace around '='
+        while(i < raw_tag.length() && std::isspace(raw_tag[i])) i++;
+        
+        if(i < raw_tag.length() && raw_tag[i] == '=') {
+            i++; // skip '='
+            while(i < raw_tag.length() && std::isspace(raw_tag[i])) i++; // skip space after '='
+            
+            std::string val;
+            // Handle quoted values like class="my-class"
+            if(i < raw_tag.length() && (raw_tag[i] == '"' || raw_tag[i] == '\'')) {
+                char quote = raw_tag[i];
+                i++; // skip opening quote
+                size_t val_start = i;
+                while(i < raw_tag.length() && raw_tag[i] != quote) i++;
+                val = raw_tag.substr(val_start, i - val_start);
+                if(i < raw_tag.length()) i++; // skip closing quote
+            } else {
+                // Handle unquoted values like width=100
+                size_t val_start = i;
+                while(i < raw_tag.length() && !std::isspace(raw_tag[i])) i++;
+                val = raw_tag.substr(val_start, i - val_start);
+            }
+            attrs[key] = val;
+        } else {
+            // Boolean attribute like <input disabled>
+            if(!key.empty()) attrs[key] = "";
+        }
     }
-    return raw_tag.substr(0, space_pos);
+    
+    return {tag_name, attrs};
 }
 
 std::unique_ptr<ElementNode> Parser::parse() {
-    // 1. Create the absolute root of our tree
     auto document = std::make_unique<ElementNode>("document");
-    
-    // 2. Create a stack to keep track of nested tags
     std::stack<ElementNode*> node_stack;
     node_stack.push(document.get());
 
-    // 3. Loop through all tokens
     while (pos < tokens.size()) {
         Token token = tokens[pos];
         pos++;
 
         if (token.type == TokenType::EOF_TOKEN) break;
 
-        // A. Handle Raw Text
         if (token.type == TokenType::TEXT) {
-            // Ignore pure empty whitespace/newlines for cleaner tree output
             if (token.value.find_first_not_of(" \n\t\r") != std::string::npos) {
                 node_stack.top()->children.push_back(
                     std::make_unique<TextNode>(token.value)
                 );
             }
         } 
-        // B. Handle Opening Tags
         else if (token.type == TokenType::START_TAG) {
-            std::string tag_name = extract_tag_name(token.value);
+            // Use C++17 structured binding to unpack the tag name and attributes
+            auto [tag_name, attributes] = parse_tag(token.value);
+            
             auto element = std::make_unique<ElementNode>(tag_name);
+            element->attributes = attributes; // Inject the attributes into the DOM node
             
-            // We need a raw pointer to put in the stack, 
-            // but unique_ptr retains ownership in the children vector!
             ElementNode* raw_ptr = element.get();
-            
-            // Add as child to the current parent, then make it the new parent
             node_stack.top()->children.push_back(std::move(element));
             node_stack.push(raw_ptr);
         } 
-        // C. Handle Closing Tags
         else if (token.type == TokenType::END_TAG) {
-            // Pop the current parent off the stack, returning to the previous parent
-            // (Don't pop the root 'document' node!)
             if (node_stack.size() > 1) {
                 node_stack.pop();
             }

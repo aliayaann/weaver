a8454e0631f815a7821e1cde6a3928ab307ab1f7
aliayaann <aliayaan.personal@gmail.com>
2026-08-16 11:25:52 +0500

    test(main): add complex HTML with attributes to verify parser

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,25 @@
 #include "html/parser.h"
 
 int main() {
-    std::cout << "--  Weaver Web Engine --" << std::endl;
+    std::cout << "--- My Browser Engine Started ---" << std::endl;
 
-    // Raw HTML from the internet
-    std::string html = "<html><body><h1>Hello World</h1><p>This is my browser.</p></body></html>";
+    // Real-world HTML with attributes!
+    std::string html = R"(
+        <html>
+            <body>
+                <h1 class="title" id="main-heading">Hello World</h1>
+                <p>This is my browser.</p>
+                <a href="https://example.com" target=_blank>Click me</a>
+            </body>
+        </html>
+    )";
     
-    // Tokenizing
     Lexer lexer(html);
     std::vector<Token> tokens = lexer.tokenize();
 
-    // Parse
     Parser parser(tokens);
     std::unique_ptr<ElementNode> dom_tree = parser.parse();
 
-    // Printing the tree
     std::cout << "\n--- DOM Tree Generated ---" << std::endl;
     dom_tree->print(0);
 
